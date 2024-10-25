1c30c3a1d0b1dc2ffebb0830ab8734b2f03508e3
Francisco Pereira <fransavastanocp@gmail.com>
2024-10-24 23:08:40 -0300

    feat(pdf): feito layout basico do pdf

--- src/utils/create-pdf.ts
+++ src/utils/create-pdf.ts
@@ -1,57 +1,56 @@
 import 'pdf-creator-node'
 import fs from 'fs'
 
-
 export async function CreatePdf() {
-    
-    const pdf = require("pdf-creator-node");
-    var html = fs.readFileSync("./src/utils/pdf-model.html", "utf8");    
-    var users = [
-        {
-            nome: "Francisco",
-            user_role: "Tecnico",
-        },
-        {
-            nome: "Sheldon",
-            user_role: "Operador",
-        },
-        {
-            nome: "Bruno",
-            user_role: "Comercial",
-        },
-    ];
-    var options = {
-        format: "A3",
-        orientation: "portrait",
-        border: "10mm",
-        header: {
-            height: "45mm",
-            contents: '<div style="text-align: center;">DMSYS</div>'
-        },
-        footer: {
-            height: "28mm",
-            contents: {
-                first: 'Cover page',
-                2: 'Second page', // Any page number is working. 1-based index
-                default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
-                last: 'Last Page'
-            }
-        }
-    };
-    var document = {
-        html: html,
-        data: {
-        users: users,
-        },
-        path: "./output.pdf",
-        type: "",
-    };
-    pdf
+  const pdf = require('pdf-creator-node')
+  var html = fs.readFileSync('./src/utils/pdf-model.html', 'utf8')
+  var users = [
+    {
+      nome: 'Francisco',
+      user_role: 'Tecnico',
+    },
+    {
+      nome: 'Sheldon',
+      user_role: 'Operador',
+    },
+    {
+      nome: 'Bruno',
+      user_role: 'Comercial',
+    },
+  ]
+  var options = {
+    format: 'A3',
+    orientation: 'portrait',
+    border: '10mm',
+    header: {
+      height: '45mm',
+      contents: '<div style="text-align: center;">DMSYS</div>',
+    },
+    footer: {
+      height: '28mm',
+      contents: {
+        first: 'Cover page',
+        2: 'Second page', // Any page number is working. 1-based index
+        default:
+          '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+        last: 'Last Page',
+      },
+    },
+  }
+  var document = {
+    html: html,
+    data: {
+      users: users,
+    },
+    path: './output.pdf',
+    type: '',
+  }
+  pdf
     .create(document, options)
     .then((res: any) => {
-        console.log(res);
+      console.log(res)
     })
     .catch((error: any) => {
-        console.error(error);
-    });
+      console.error(error)
+    })
 }
