c1b94ecf06cf0668443db16378dd98ce45540636
Francisco Pereira <fransavastanocp@gmail.com>
2024-12-27 11:11:08 -0300

    feat(cliente): ajustada algumas processos para melhor cadastrar os clientes

--- src/use-cases/factories/make-check-session-use-case.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaSessionRepository } from '@/repositories/prisma/prisma-session-repository'
-import { CheckSessionUseCase } from './check-session'
-
-export function makeDeleteDutyUseCase() {
-  const sessionRepository = new PrismaSessionRepository()
-  const checkSessionUseCase = new CheckSessionUseCase(sessionRepository)
-
-  return checkSessionUseCase
-}
