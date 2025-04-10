b05945d771bfd3d116f1542dff8d118dc2966dd9
Francisco Savastano Cazati Pereira <fransavastanocp@gmail.com>
2025-04-10 11:09:54 -0300

    feat(api v1): adicionada v1 a path de api

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const allowedOrigins = [
   'http://192.168.10.246',
   'http://192.168.10.246:80',
   'http://dmsys.app.ci',
-  'https://dmsysci01.ddns.me',
+  'http://dmsysci01.ddns.me',
   'http://200.142.98.17', //local microtik ip mapping
 ]
 
